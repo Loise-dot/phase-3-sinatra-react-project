b293b122f10fc724cecaa11a03447b9a3a8225e4
Loise <loise.wambui@student.moringaschool.com>
2023-03-07 05:05:05 +0300

    Create blogpost table

--- db/migrate/20230305222027_create_blogposts.rb
+++ db/migrate/20230305222027_create_blogposts.rb
@@ -1,11 +1,11 @@
-class CreateBlogposts < ActiveRecord::Migration[6.1]
-  def change
-    create_table :blogposts do |t|
-      t.string :title
-      t.text :content
-      t.string :url
-      t.timestamps
+# class CreateBlogposts < ActiveRecord::Migration[6.1]
+#   def change
+#     create_table :blogposts do |t|
+#       t.string :title
+#       t.text :content
+#       t.string :url
+#       t.timestamps
       
-    end
-  end
-end
+#     end
+#   end
+# end
